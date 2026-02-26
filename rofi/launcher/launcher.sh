d645984b09093640ca2ea0bd5cd35cc1c682fdbe
Buscalune <ryanb13103@gmail.com>
2026-02-26 21:04:24 +0000

    Move configs into .config directory

--- rofi/launcher/launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-# Directory containing the theme
-dir="${HOME}/.config/rofi/launcher"
-theme="launcher"
-
-# Run Rofi with the custom theme
-rofi -show drun -theme "${dir}/${theme}.rasi"
